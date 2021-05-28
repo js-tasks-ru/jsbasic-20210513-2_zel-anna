2ec12e7ffad23b9946772908581835f9a8983a30
Anya <anya@zelibobook.local>
2021-05-28 20:53:18 +0300

    3-module-4-5-tasks solved

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
-  // ваш код...
+  let newArrays = [];
+  let userNameSalary;
+
+  for (let user of users) {
+    if (user.age <= age) {
+      userNameSalary = user.name + ', ' + user.balance;
+      newArrays.push(userNameSalary);
+    } 
+  }
+    
+  return newArrays.join('\n');
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,15 @@
 function getMinMax(str) {
-  // ваш код...
+  let result = {};
+  let numbers = [];
+  let min;
+  let max;
+
+  numbers = str.replace(/[^\d\-\.]/g, ',').split(",").filter(a => a.length).map(a => +a);
+  
+  min = Math.min(...numbers);
+  max = Math.max(...numbers);
+  result.min = min;
+  result.max = max;
+  
+  return result;
 }
