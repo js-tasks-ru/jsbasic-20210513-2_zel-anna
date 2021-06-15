c6754062639220c81b17329a17a9f00c832fd04e
Anya <anya@zelibobook.local>
2021-06-15 20:51:28 +0300

    7-module-1-task fixed

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -4,89 +4,64 @@ import categories from './categories.js';
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
-
+    this.ribbonInner = document.createElement('nav');
     this.elem = document.createElement('div');
     this.elem.classList.add('ribbon');
     this.render(categories);
 
     this.elem.addEventListener('click', (event) => this.onClick(event));
-
+    this.ribbonInner.addEventListener( "scroll" , (event) => this.onScroll(event));
   }
 
   render(categories){
-    let btn_left = document.createElement('button');
-    this.elem.append(btn_left);
-    btn_left.classList.add('ribbon__arrow');
-    btn_left.classList.add('ribbon__arrow_left'); 
-    
-    btn_left.innerHTML = `<img src="/assets/images/icons/angle-icon.svg" alt="icon">`;
+    this.btnLeft = document.createElement('button');
+    this.elem.append(this.btnLeft);
+    this.btnLeft.classList.add('ribbon__arrow');
+    this.btnLeft.classList.add('ribbon__arrow_left');
+    this.btnLeft.innerHTML = `<img src="/assets/images/icons/angle-icon.svg" alt="icon">`;
 
-    let ribbonInner = document.createElement('nav');
-    this.elem.append(ribbonInner);
-    ribbonInner.classList.add('ribbon__inner');
+    //let ribbonInner = document.createElement('nav');
+    this.elem.append(this.ribbonInner);
+    this.ribbonInner.classList.add('ribbon__inner');
 
     categories.forEach(category => {
-      ribbonInner.innerHTML += `<a href="#" class="ribbon__item" data-id="${category.id}">${category.name}</a>\n`;
+      this.ribbonInner.innerHTML += `<a href="#" class="ribbon__item" data-id="${category.id}">${category.name}</a>\n`;
     });
 
-    let btn_right = document.createElement('button');
-    this.elem.append(btn_right);
-    btn_right.classList.add('ribbon__arrow');
-    btn_right.classList.add('ribbon__arrow_right'); 
-    btn_right.classList.add('ribbon__arrow_visible');
-    btn_right.innerHTML = `<img src="/assets/images/icons/angle-icon.svg" alt="icon">`;
-    
-    ribbonInner.onscroll = function(){
-      let scrollWidth = ribbonInner.scrollWidth;
-      let scrollLeft = ribbonInner.scrollLeft;
-      let clientWidth = ribbonInner.clientWidth;
-      let scrollRight = scrollWidth - scrollLeft - clientWidth;   
-      //console.log('scrollWidth:', scrollWidth, 'scrollLeft:', scrollLeft,'clientWidth:', clientWidth,'scrollRight:', scrollRight );
-      
-      if (scrollLeft === 0) {
-        btn_left.classList.remove('ribbon__arrow_visible');
-      } else {
-        btn_left.classList.add('ribbon__arrow_visible');
-      }
-      if (scrollRight < 1) {
-        btn_right.classList.remove('ribbon__arrow_visible');
-      } else {
-        btn_right.classList.add('ribbon__arrow_visible');
-      }
-    } 
+    this.btnRight = document.createElement('button');
+    this.elem.append(this.btnRight);
+    this.btnRight.classList.add('ribbon__arrow');
+    this.btnRight.classList.add('ribbon__arrow_right'); 
+    this.btnRight.classList.add('ribbon__arrow_visible');
+    this.btnRight.innerHTML = `<img src="/assets/images/icons/angle-icon.svg" alt="icon">`;
 
-    btn_left.onclick = function() {
-      ribbonInner.scrollBy(-350, 0);
+    this.btnLeft.onclick = () => {
+      this.ribbonInner.scrollBy(-350, 0);
     }
-    btn_right.onclick = function() {
-      ribbonInner.scrollBy(350, 0);
+    this.btnRight.onclick = () => {
+      this.ribbonInner.scrollBy(350, 0);
     }
+  }
     
-    
-    ribbonInner.onclick = function(event) {
+    onClick(event) {
       if (event.target.nodeName != 'A') return;
       
       event.preventDefault();
-      let activeItem = ribbonInner.querySelector('.ribbon__item_active');
+      let activeItem = this.ribbonInner.querySelector('.ribbon__item_active');
 
       if (activeItem) {
         activeItem.classList.remove('ribbon__item_active');
       }
       
       event.target.classList.add('ribbon__item_active');
-      
-    }; 
 
 
-  }
-    
-    onClick(event) {
       
       let chosenCategory = event.target.closest('.ribbon__item');
       if (!chosenCategory) {
         return;
       }
-      console.log(chosenCategory.getAttribute('data-id'));
+      //console.log(chosenCategory.getAttribute('data-id'));
 
       let customEvent = new CustomEvent("ribbon-select", { 
         detail: chosenCategory.getAttribute('data-id'), 
@@ -95,10 +70,26 @@ export default class RibbonMenu {
       this.elem.dispatchEvent(customEvent);
   
       
-      console.log(customEvent);
+      //console.log(customEvent);
     }
-    
-    
+
+    onScroll (event) {
+         let scrollWidth = this.ribbonInner.scrollWidth;
+         let scrollLeft = this.ribbonInner.scrollLeft;
+         let clientWidth = this.ribbonInner.clientWidth;
+         let scrollRight = scrollWidth - scrollLeft - clientWidth;
+
+         if (scrollLeft === 0) {
+           this.btnLeft.classList.remove('ribbon__arrow_visible');
+         } else {
+          this.btnLeft.classList.add('ribbon__arrow_visible');
+         }
+         if (scrollRight < 1) {
+          this.btnRight.classList.remove('ribbon__arrow_visible');
+         } else {
+          this.btnRight.classList.add('ribbon__arrow_visible');
+         }
+     }
     
 }  
 
